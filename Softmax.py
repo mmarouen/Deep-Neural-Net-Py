7ea4690ac472b54a27757a29e4236b398443f02c
azzouz marouen <azzouz.marouen@gmail.com>
2017-09-13 20:26:10 +0200

    Create Softmax.py

---
+++ Softmax.py
@@ -0,0 +1,9 @@
+def Softmax(X):
+    X=np.matrix(X)
+    eps=1e-15
+    Eps=1-eps
+    M=np.max(X)
+    prod=np.apply_along_axis(lambda x: np.exp(x-M)/np.sum(np.exp(X-M),axis=1),0,X)
+    prod[prod>Eps]=Eps
+    prod[prod<eps]=eps
+    return(prod)
